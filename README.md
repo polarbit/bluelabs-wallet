7a14059b0a97ca35fae3ff8381a80ffc40e73a06
Şafak Ulusoy <safakulusoy@gmail.com>
2021-08-31 23:32:47 +0000

    Trivial changes

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 # Install 
 go get github.com/AsynkronIT/protoactor-go
+
+
+# temp
+https://github.com/protocolbuffers/protobuf
+https://github.com/protocolbuffers/protobuf-go
--- main.go
+++ main.go
@@ -11,9 +11,18 @@ type helloActor struct{}
 
 func (state *helloActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
+	case *actor.Started:
+		fmt.Println("Started, initialize actor here")
+	case *actor.Stopping:
+		fmt.Println("Stopping, actor is about shut down")
+	case *actor.Stopped:
+		fmt.Println("Stopped, actor and its children are stopped")
+	case *actor.Restarting:
+		fmt.Println("Restarting, actor is about restart")
 	case *hello:
 		fmt.Printf("Hello %v\n", msg.Who)
 		ch <- true
+		fmt.Println("Roger!")
 	}
 }
 
@@ -21,11 +30,14 @@ var ch chan bool
 
 func main() {
 	ch = make(chan bool, 1)
+
 	system := actor.NewActorSystem()
 	props := actor.PropsFromProducer(func() actor.Actor { return &helloActor{} })
-
 	pid := system.Root.Spawn(props)
+
+	// Send operations are non-blocking
 	system.Root.Send(pid, &hello{Who: "You"})
 	system.Root.Send(pid, &hello{Who: "You Too"})
 	<-ch
+	system.Root.StopFuture(pid).Wait()
 }
